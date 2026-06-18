43dcf327006de9516f45cb038947a4735773f7f9
Badal Kumar <badalkumar9910@gmail.com>
2026-06-18 19:25:20 +0530

    initial WanderLust project

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const Listing = require("./models/listing.js");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
+
+
+app.engine("ejs",ejsMate);
+app.set("views",path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public")));
+app.set("view engine","ejs");
+
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+main().then(()=>{
+    console.log("connected to DB");
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+}
+
+app.get("/",(req,res)=>{
+    res.send("home");
+});
+
+
+
+app.get("/listings",async(req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
+});
+
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+});
+
+app.post("/listings/new",async(req,res)=>{
+    let newListing = new Listing(req.body.listing);
+    console.log(newListing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id} = req.params;
+    let listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+});
+
+app.put("/listings/:id/edit",async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,req.body.listing);
+    res.redirect(`/listings/${id}`);
+
+});
+
+app.delete("/listings/:id",async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+});
+
+
+app.get("/listings/:id",async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+});
+
+app.listen(8080,()=>{
+    console.log("listening on 8080");
+});
